a0213fd0a00ec39f73ee3f370d86597e3e32e316
julianpipecardenas-netizen <julianpipecardenas@gmail.com>
2026-04-24 15:33:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const cors         = require('cors');
 const swaggerUi    = require('swagger-ui-express');
 const swaggerSpec  = require('./swagger');
 
-const estudiantesRouter = require('./routes/estudiantes');
-const notasRouter       = require('./routes/notas');
+const estudiantesRouter = require('./estudiantes');
+const notasRouter       = require('./notas');
 
 const app  = express();
 const PORT = process.env.PORT || 3000;
@@ -13,28 +13,23 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-// Swagger UI en /api-docs
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
   customSiteTitle: 'NotasApp API Docs',
   customCss: '.swagger-ui .topbar { background-color: #1a237e; }',
 }));
 
-// Spec JSON para importar en Postman
 app.get('/api-docs.json', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
   res.send(swaggerSpec);
 });
 
-// Health check
 app.get('/', (req, res) => {
   res.json({ status: 'ok', service: 'notas-microservicio', version: '1.0.0', docs: '/api-docs' });
 });
 
-// Rutas
 app.use('/api/estudiantes', estudiantesRouter);
 app.use('/api/notas',       notasRouter);
 
-// 404
 app.use((req, res) => {
   res.status(404).json({ message: 'Ruta no encontrada' });
 });
